b6e40a366d220c926e8866ba5393c0a74e3659f4
Deviego <diegodomingues266@gmail.com>
2022-05-11 13:30:19 -0300

    banco create and trasport created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,49 @@
-import express from 'express'
+// imports
+import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
-let app = express()
+let app = express();
+app.use(express.json());
+// code
 
-app.get('/user', (req,res) =>{
-    return res.send('hello world')
-})
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4314c7dfdaa24d",
+    pass: "0b6951cbe9f75c",
+  },
+});
 
-app.listen(3333, () => console.log('server is running'))
+// Rotas
 
+app.post("/feedback", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  })
+
+  await transport.sendMail({
+    from: "Equipe Feeget <oi@feedget.com>",
+    to: "Deviego <diegodomingues266@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do Feedback: ${type}</p>`,
+      `<p> Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
+
+app.listen(3000, () => console.log("server is running"));
 
 // SQlite
-//prosma
+//prisma
