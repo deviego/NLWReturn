2b2f44d3f0a95bd34ae0eeb0707e5847525122e1
Deviego <diegodomingues266@gmail.com>
2022-05-14 15:13:03 -0300

    intial intetgration and finish  backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 // imports
 import express from "express";
 import { routes } from "./routes";
+import cors from 'cors'
 
 let app = express();
 
 app.use(express.json());
 
-
+app.use(cors())
 
 // Rotas
 app.use(routes)
